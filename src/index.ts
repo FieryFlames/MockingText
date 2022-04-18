2928086b4443c8f905ea375adadb73dc47df44cb
Jack Matthews <jm5112356@gmail.com>
2022-04-17 22:17:21 -0400

    Initial mockify

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,51 @@
+import { ApplicationCommandInputType, ApplicationCommandOptionType, ApplicationCommandType, Command, EnmitySectionID } from "enmity-api/commands";
 import { Plugin, registerPlugin } from "enmity-api/plugins";
 
-const ExamplePlugin: Plugin = {
-  name: "ExamplePlugin",
+const mockify: Plugin = {
+  name: "mockify",
   commands: [],
-  patches: [],
 
   onStart() {
+    const mock: Command = {
+      id: "mock-command",
+      applicationId: EnmitySectionID,
 
+      name: "mock",
+      displayName: "mock",
+
+      description: "eXamPlE tExT",
+      displayDescription: "eXamPlE tExT",
+
+      type: ApplicationCommandType.Chat,
+      inputType: ApplicationCommandInputType.BuiltInText,
+
+      options: [{
+        name: "text",
+        displayName: "text",
+
+        description: "Text to mock",
+        displayDescription: "Text to mock",
+
+        type: ApplicationCommandOptionType.String,
+        required: true
+      }],
+
+      execute: function (args, message) {
+        const text = args[0].value;
+        const mockedText = text.split('').map(c => Math.random() < 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+
+        return {
+          content: mockedText
+        }
+      }
+    }
+    
+    this.commands.push(mock);
   },
 
   onStop() {
-
+    this.commands = [];
   }
 }
 
-registerPlugin(ExamplePlugin);
+registerPlugin(mockify);
